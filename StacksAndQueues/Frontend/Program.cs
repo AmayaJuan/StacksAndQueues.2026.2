d28e783d2e329cfa4f38578d586cac540b6d47be
Juan Pablo Velez Amaya <jpva360@gmail.com>
2026-09-06 21:36:57 -0500

    Stack Using  Arrary

---
+++ StacksAndQueues/Frontend/Program.cs
@@ -0,0 +1,51 @@
+﻿using Backend;
+
+var stack = new StackUsingArray<string>(10);
+
+
+var option = string.Empty;
+do
+{
+    try
+    {
+        option = Menu();
+        switch (option)
+        {
+            case "1":
+                Console.Write("Digite el elemento: ");
+                stack.Push(Console.ReadLine()!);
+                break;
+            case "2":
+                Console.WriteLine($"Elemento desapilado: {stack.Pop()}");
+                break;
+            case "3":
+                Console.WriteLine($"Elemento en el tope de la pila: {stack.Peek()}");
+                break;
+            case "0":
+                Console.WriteLine("Saliendo del programa...");
+                break;
+            default:
+                Console.WriteLine("Opción no inválida.");
+                break;
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
+    }
+} while (option != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Apilar");
+    Console.WriteLine("2. Desapilar");
+    Console.WriteLine("3. Ver tope de la pila");
+    Console.WriteLine("0. Salir");
+    Console.Write("Digite su opción: ");
+    return Console.ReadLine()!;
+}
